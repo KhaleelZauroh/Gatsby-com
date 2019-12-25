3c0a41c295c3170de4eeb0f64f5337822a7cf953
Ibrahim zauroh <46751915+KhaleelZauroh@users.noreply.github.com>
2019-12-26 04:06:49 +0800

    Getting started
    Deleted boilerplate codes and unneccesary lines.
    
    Added images and new pages/reusable components i.e footer

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Gatsby Starter`,
+    description: `we do what we do best, we improvise`,
     author: `@gatsbyjs`,
   },
   plugins: [
