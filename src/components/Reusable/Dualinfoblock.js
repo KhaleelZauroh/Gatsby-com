1cd9b85edd2c557af08b39c31fe43267fe8a8de2
Ibrahim zauroh <46751915+KhaleelZauroh@users.noreply.github.com>
2019-12-26 19:50:15 +0800

    Add in dualinfoblock (with cards)

---
+++ src/components/Reusable/Dualinfoblock.js
@@ -0,0 +1,60 @@
+import React from "react"
+import Heading from "./Heading"
+// import { Link } from "gatsby"
+
+export default function Dualinfoblock({ heading }) {
+  return (
+    <section className="my-4 py-4  bg-theme">
+      <div className="container">
+        <Heading title={heading} />
+        <div className="row">
+          <div className="col-8 mx-auto">
+            <p className="lead text-white mb-5">
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+              commodo ligula eget dolor. Aenean massa. Cum sociis natoque
+              penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+              Donec quam felis, ultricies nec, pellentesque eu, pretium quis,
+              sem. Nulla consequat massa quis enim. Donec pede justo, fringilla
+              vel, aliquet nec, vulputate eget, arcu. Lorem ipsum dolor sit
+              amet, consectetuer adipiscing elit. Aenean commodo ligula eget
+              dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis
+              parturient montes, nascetur ridiculus mus. Donec quam felis,
+              ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat
+              massa quis enim. Donec pede justo, fringilla vel, aliquet nec,
+              vulputate eget, arcu. Lorem ipsum dolor sit amet, consectetuer
+              adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.
+              Cum sociis natoque penatibus et magnis dis parturient montes,
+              nascetur ridiculus mus. Donec quam felis, ultricies nec,
+              pellentesque eu, pretium quis, sem. Nulla consequat massa quis
+              enim. Donec pede justo, fringilla vel, aliquet nec, vulputate
+              eget, arcu.
+            </p>
+          </div>
+          <div className="col-4">
+            <div class="card bg-dark">
+              <img
+                class="card-img-top"
+                src="https://images.hdqwalls.com/wallpapers/bthumb/borderlands-3-minimalist-4k-53.jpg"
+                alt="image goes here"
+              />
+              <div class="card-body">
+                <h5 class="card-title text-success">Click Photos</h5>
+                <p class="card-text text-white">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content. Some quick example text to
+                  build on the card title and make up the bulk of the card's
+                  content. Some quick example text to build on the card title
+                  and make up the bulk of the card's content.
+                </p>
+                <a href="#" class="btn btn-warning btn-block">
+                  Go somewhere
+                  {heading}
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Section from "../components/Reusable/Section"
 import Infoblock from "../components/Reusable/Infoblock"
+import Dualinfoblock from "../components/Reusable/Dualinfoblock"
 
 
 const IndexPage = ({data}) => (
@@ -18,6 +19,7 @@ const IndexPage = ({data}) => (
      />
 
     <Infoblock heading="About Us" />
+    <Dualinfoblock heading="Our Team" />
   </Layout>
 )
 
