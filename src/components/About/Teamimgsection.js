746897f9181e9df074050361255e6b53cda3f5a1
Ibrahim zauroh <46751915+KhaleelZauroh@users.noreply.github.com>
2019-12-29 15:33:33 +0800

    fix classname errors

--- src/components/About/Teamimgsection.js
+++ src/components/About/Teamimgsection.js
@@ -8,38 +8,38 @@ export default function Teamimgsection({heading}) {
     <Heading title={heading} />
     <div className="row">
       <div className="col-10 col-sm-10 mx-auto">
-        <div class="card-group">
-          <div class="card">
+        <div className="card-group">
+          <div className="card">
             <img
               src="https://render.fineartamerica.com/images/rendered/search/print/images-medium-5/frida-kahlo-elena-day.jpg"
-              class="card-img-top"
+              className="card-img-top"
               alt="..."
             />
-            <div class="card-body">
-              <h5 class="card-title">Frida khalo</h5>
-              <p class="card-text">
+            <div className="card-body">
+              <h5 className="card-title">Frida khalo</h5>
+              <p className="card-text">
                 This is a wider card with supporting text below as a natural
                 lead-in to additional content. This content is a little bit
                 longer.
               </p>
-              <p class="card-text">
+              <p className="card-text">
                 <small class="text-muted">Last updated 3 mins ago</small>
               </p>
             </div>
           </div>
-          <div class="card">
+          <div className="card">
             <img
               src="https://render.fineartamerica.com/images/rendered/search/print/images-medium-5/self-portrait-balazs-solti.jpg"
-              class="card-img-top"
+              className="card-img-top"
               alt="..."
             />
-            <div class="card-body">
-              <h5 class="card-title">Astronaut</h5>
-              <p class="card-text">
+            <div className="card-body">
+              <h5 className="card-title">Astronaut</h5>
+              <p className="card-text">
                 This card has supporting text below as a natural lead-in to
                 additional content.
               </p>
-              <p class="card-text">
+              <p className="card-text">
                 <small class="text-muted">Last updated 3 mins ago</small>
               </p>
             </div>
@@ -50,15 +50,15 @@ export default function Teamimgsection({heading}) {
               class="card-img-top"
               alt="..."
             />
-            <div class="card-body">
-              <h5 class="card-title">Trapped</h5>
-              <p class="card-text">
+            <div className="card-body">
+              <h5 className="card-title">Trapped</h5>
+              <p className="card-text">
                 This is a wider card with supporting text below as a natural
                 lead-in to additional content. This card has even longer content
                 than the first to show that equal height action.
               </p>
-              <p class="card-text">
-                <small class="text-muted">Last updated 3 mins ago</small>
+              <p className="card-text">
+                <small className="text-muted">Last updated 3 mins ago</small>
               </p>
             </div>
           </div>
